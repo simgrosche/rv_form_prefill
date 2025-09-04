65a9a0d121f9b42a24f3e8a713400ccd8990b00d
simgrosche <180856769+simgrosche@users.noreply.github.com>
2025-09-04 10:57:29 +0200

    Move family definition into yaml file

--- rv_form_prefill/my_family_definition.py
+++ rv_form_prefill/my_family_definition.py
@@ -1,47 +1,20 @@
+import yaml
+from pathlib import Path
 from rv_form_prefill.models.family import Family
 from rv_form_prefill.models.person import Mother, Father, Child
 from rv_form_prefill.models.residence import Residence
 
 
 def get_my_family_definition() -> Family:
-    residence = Residence(
-        street="Main Street",
-        street_number="1",
-        plz="12345",
-        city="Berlin",
-        country="Deutschland",
-    )
-    mother = Mother(
-        first_name="Erika",
-        last_name="Musterfrau",
-        birth_name="Musterfrau",
-        birth_date="01.01.1990",
-        birth_city="Berlin-Spandau",
-        birth_country="DeutschlandMother",
-        rentenversicherungsnummer="12345678",
-    )
-    father = Father(
-        first_name="Max",
-        last_name="Musterfrau",
-        birth_name="Mustermann",
-        birth_date="12.12.1991",
-        birth_city="Potsdam",
-        birth_country="DeutschlandFather",
-        rentenversicherungsnummer="9876543",
-    )
-    child1 = Child(
-        first_name="Tim",
-        last_name="Musterkind",
-        birth_date="01.05.2020",
-        birth_city="Berlin1",
-        birth_country="Deutschland1",
-    )
-    child2 = Child(
-        first_name="Tom",
-        last_name="Musterkind2",
-        birth_date="03.06.2020",
-        birth_city="Berlin2",
-        birth_country="Deutschland2",
-    )
+    yaml_path = Path(__file__).parent.parent / 'family_definition.yaml'
+    with open(yaml_path) as f:
+        data = yaml.safe_load(f)['family']
+    
+    residence = Residence(**data['residence'])
+    mother = Mother(**data['mother'])
+    father = Father(**data['father'])
+    children = [Child(**child_data) for child_data in data['children']]
+    
+    return Family(residence, mother, father, *children)
 
-    return Family(residence, mother, father, child1, child2)
+
